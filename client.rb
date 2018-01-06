9cc7b864e7e2989f7b9f6e7a3e6355e79e5e2ad9
tjr05d <tjr05d@my.fsu.edu>
2018-01-06 12:39:22 -0500

    Initial commit

---
+++ client.rb
@@ -0,0 +1,18 @@
+require_relative 'coin.rb'
+require 'faraday'
+
+URL = 'http://localhost'
+PORT = 4567
+
+
+def create_user name
+    Faraday.post("#{URL}:#{PORT}/users", name: name).body
+end 
+
+def get_balance user
+    Faraday.get("#{URL}:#{PORT}/balance", user: user).body
+end 
+
+def transfer from, to, amount
+    Faraday.post("#{URL}:#{PORT}/transfers", from: from, to: to, amount: amount).body
+end
---
+++ coin.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'colorize'
+
+BALANCES = {
+  'tim' => 1_000_000,
+}
+
+get "/balance" do
+  user = params['user'].downcase
+  puts BALANCES.to_s.yellow
+  "#{user} has #{BALANCES[user]}"
+end 
+
+post "/users" do
+  name = params['name'].downcase
+  BALANCES[name] ||= 0 
+  puts BALANCES.to_s.yellow
+  "OK"
+end 
+
+post "/transfers" do
+  from, to = params.values_at('from', 'to').map(&:downcase)
+  amount = params['amount'].to_i
+  #if user doesn't have enough money to transfer then raise error
+  raise unless BALANCES[from] >= amount && BALANCES[from] && BALANCES[to]
+  BALANCES[from] -= amount
+  BALANCES[to] += amount
+  puts BALANCES.to_s.yellow
+  "OK"
+end 
